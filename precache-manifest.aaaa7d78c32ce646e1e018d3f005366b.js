e0b036fb8cc493e582bd45e4bd9d6e4b7b4bb156
Alexandr Zhuk <sanek201dg@ukr.net>
2019-01-21 16:14:19 +0200

    Updates

---
+++ precache-manifest.aaaa7d78c32ce646e1e018d3f005366b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccd5e426c9bb75b1b8d4",
+    "url": "/Albert_react/static/js/runtime~main.ccd5e426.js"
+  },
+  {
+    "revision": "985dc582ad7e543d5677",
+    "url": "/Albert_react/static/js/main.985dc582.chunk.js"
+  },
+  {
+    "revision": "f4f3ceda2cd3badd5973",
+    "url": "/Albert_react/static/js/1.f4f3ceda.chunk.js"
+  },
+  {
+    "revision": "985dc582ad7e543d5677",
+    "url": "/Albert_react/static/css/main.7b0833f0.chunk.css"
+  },
+  {
+    "revision": "4580225a318ccb886eb79d9da1c4c095",
+    "url": "/Albert_react/index.html"
+  }
+];
